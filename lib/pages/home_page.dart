743c4cf55f76f07d6c177ee2eb6ad2419913c2d9
vibxop <jdpranav333@gmail.com>
2024-08-31 13:25:27 +0530

    2 commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("1st page"),
+      appBar: AppBar(title: Text("EmPower"),
       backgroundColor: Colors.deepPurple,
       ),
       bottomNavigationBar: BottomBar(),
@@ -33,16 +33,80 @@ class HomePage extends StatelessWidget{
           ],
         ),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Go to second page"),
-          onPressed: () {
-            //navigate to 2nd page
-            Navigator.pushNamed(context, '/secondpage');
-          },
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //map container
+            Container(
+              height: 450,
+              width: double.infinity,
+              color: Colors.deepPurple[300],
+
+            ),
+            //buttons container
+            Container(
+              height: 235,
+              color: Colors.deepPurple[100],
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      //button1
+                      ElevatedButton(
+                        onPressed: () {
+                          //Call Family
+                        }, 
+                        child: Text("Call Family"),
+                        
+                      ),
+                      //button2
+                      ElevatedButton(
+                        onPressed: () {
+                          //Call Family
+                        }, 
+                        child: Text("Call Police"),
+                        
+                      ),
+                    ],
+                    
+                  ),
+                  SizedBox(height: 20),
+                  //button3
+                  ElevatedButton(
+                        onPressed: () {
+                          //SOS button
+                        }, 
+                        child: Text(
+                          "S O S",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),  
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Tap SOS in case of emergency!",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    );
+      );
   }
 }
 
